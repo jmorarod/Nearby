f21fd436d6a986ed99ab5cbdf4b864d841b73e5a
José Mora <josemorar96@gmail.com>
2018-04-10 11:03:38 -0600

    Primer activity y activity login

--- app/src/main/java/cr/ac/jmorarodic_itcr/nearby/MainActivity.java
+++ app/src/main/java/cr/ac/jmorarodic_itcr/nearby/MainActivity.java
@@ -1,10 +1,39 @@
 package cr.ac.jmorarodic_itcr.nearby;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void onClickIngresar(View view){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+    public void onClickRegistrarse(View view)
+    {
+        CharSequence opciones[] = new CharSequence[] {"Ingresar con la aplicación", "Facebook"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Registro");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which == 0){
+                    //intent
+                }
+                else{
+                    Log.i("Login","Facebook");
+                    //registrar con facebook
+                }
+            }
+        });
+        builder.show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
