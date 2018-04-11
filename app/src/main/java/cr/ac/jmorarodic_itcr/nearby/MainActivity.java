5579f2aade77182439e5765e7ee5b182d593b53b
José Mora <josemorar96@gmail.com>
2018-04-10 22:51:31 -0600

    Facebook login

--- app/src/main/java/cr/ac/jmorarodic_itcr/nearby/MainActivity.java
+++ app/src/main/java/cr/ac/jmorarodic_itcr/nearby/MainActivity.java
@@ -2,20 +2,40 @@ package cr.ac.jmorarodic_itcr.nearby;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    CallbackManager callbackManager;
+    LoginButton loginButton;
     public void onClickIngresar(View view){
         Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
     public void onClickRegistrarse(View view)
     {
+        //intent
+        Log.i("Registro","RegistroApp");
+        /*
         CharSequence opciones[] = new CharSequence[] {"Ingresar con la aplicación", "Facebook"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -25,18 +45,65 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 if(which == 0){
                     //intent
+                    Log.i("Registro","RegistroApp");
                 }
                 else{
                     Log.i("Login","Facebook");
-                    //registrar con facebook
+
                 }
             }
         });
-        builder.show();
+        builder.show();*/
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile"));
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
